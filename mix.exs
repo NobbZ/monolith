fb7c7d7f59bf7c80daf8f2b7269f23c4c948ec8e
Norbert Melzer <timmelzer@gmail.com>
2016-03-17 00:05:43 +0100

    Some handy aliases

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Monolith.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     aliases: aliases,
      deps: deps]
+  end
+
+  def aliases do
+    [c: ["deps.get", "deps.compile", "compile", "docs"],
+     r: ["clean", "c"]]
   end
 
   # Configuration for the OTP application
@@ -28,8 +34,8 @@ defmodule Monolith.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.2.1"},
+     {:todo,   "~> 1.1.0"},
 
-     {:todo,   "~> 1.1.0",  only: :dev},
      {:ex_doc, "~> 0.11.4", only: :dev}]
   end
 end
