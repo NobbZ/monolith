82b883e2ef8267744b5dc72d99eaa84f68134a37
Norbert Melzer <timmelzer@gmail.com>
2016-03-17 00:31:54 +0100

    no retval...

--- lib/monolith/project.ex
+++ lib/monolith/project.ex
@@ -2,12 +2,12 @@ defmodule Monolith.Project do
   require Logger
 
   def init() do
-    :ok = create_folders()
+    create_folders()
   end
 
   defp create_folders() do
     folders = Enum.map(["posts", "drafts", "pages", "templates"], &("./priv/#{&1}"))
-    result = for folder <- folders do
+    for folder <- folders do
       Logger.info "Creating `#{folder}`"
       case File.mkdir_p(folder) do
         {:error, reason} ->
@@ -16,6 +16,5 @@ defmodule Monolith.Project do
         :ok -> :ok
       end
     end
-    if Enum.all?(result, &(&1 === :ok)), do: :ok, else: :error
   end
 end
